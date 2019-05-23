5d34b1f9dd41335af981f0a2beeadb997e945d37
Ashna Shah-Grover <as19@williams.edu>
2019-05-23 08:34:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,13 +1,29 @@
+require 'pry'
+
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |item_hash|
+    item_hash.each do |item_name, info_hash|
+      new_hash[item_name] ||= info_hash
+      new_hash[item_name][:count] ||= 0
+      new_hash[item_name][:count] += 1 
+    end
+  end 
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
-end
+end 
+
+
 
 def apply_clearance(cart)
-  # code here
+  cart.each_with_object({}) do |(key, value), hash|
+    hash ||= cart 
+    if value[:clearance] == true 
+      hash[key][:price].to_f *= 0.8
+    end 
+  end 
 end
 
 def checkout(cart, coupons)
